05500b8bd033665cab3fa06c51191453fb050a16
Anthony Wan <Anthonywan00@gmail.com>
2019-06-25 16:11:50 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,23 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
+    # @genre = genre
     @title = title
+    # @author = author
+    # @page_count = page_count
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
 
+  # 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
